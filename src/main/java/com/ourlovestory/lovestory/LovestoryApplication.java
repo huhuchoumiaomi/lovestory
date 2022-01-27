cf55891370536285c4b681b7e196ce1738f196cb
huhuchoumiaomi <huhuchoumiao@163.com>
2022-01-27 18:53:31 +0800

    init repo

---
+++ src/main/java/com/ourlovestory/lovestory/LovestoryApplication.java
@@ -0,0 +1,13 @@
+package com.ourlovestory.lovestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovestoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovestoryApplication.class, args);
+    }
+
+}
